165e793e40f78bfb12d4fd73a0e2c2e5acfe8b7c
Ingwar <Ingwar@Igor-PC>
2017-05-09 10:41:31 +0200

    correction d'une partie des erreurs de minimax

--- MetaMorpion/src/jeu/algosIA/Minimax.java
+++ MetaMorpion/src/jeu/algosIA/Minimax.java
@@ -24,25 +24,29 @@ public class Minimax extends Algorithm {
 		
 		ArrayList<Integer> colonne = new ArrayList<Integer>();
 		
-		for (int i = 0; i < Constantes.NB_COLONNES; i++){
-			if (grilleDepart.isCoupPossible(i)){
-				colonne.add(i+1);
+		for (int i = 0; i < 9; i++){
+			for (int j = 0; j < 9; j++){
+				if (grilleDepart.isCoupPossible(i,j)){
+					colonne.add(i+1);
+				}
 			}
 		}
 		
 		double valeur = Constantes.SCORE_MIN_NON_DEFINI;
-		for (int i = 0; i < Constantes.NB_COLONNES; i++) {
-			if (grilleDepart.isCoupPossible(i)) {
-				BigGrille grillebis = grilleDepart.clone();
-				grillebis.ajouterCoup(i, symboleMax);
-				double valeurCourante = min(grillebis, tourDepart);
-				if (valeurCourante > valeur) {
-					colonne.clear();
-					colonne.add(i);
-					valeur = valeurCourante;
-				}
-				if (valeurCourante == valeur){
-					colonne.add(i);
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++){
+				if (grilleDepart.isCoupPossible(i, j)) {
+					BigGrille grillebis = grilleDepart.clone();
+					grillebis.ajouterCoup(i,j, symboleMax);
+					double valeurCourante = min(party, grillebis, tourDepart);
+					if (valeurCourante > valeur) {
+						colonne.clear();
+						colonne.add(i);
+						valeur = valeurCourante;
+					}
+					if (valeurCourante == valeur){
+						colonne.add(i);
+					}
 				}
 			}
 		}
@@ -57,11 +61,13 @@ public class Minimax extends Algorithm {
 
 
 			double valeur = Constantes.SCORE_MAX_NON_DEFINI;
-			for (int i = 0; i < Constantes.NB_COLONNES; i++) {
-				if (grille.isCoupPossible(i)){
-					Grille grillebis = grille.clone();
-					grillebis.ajouterCoup(i, symboleMin);
-					valeur = Math.min(valeur, this.max(grillebis, tour+1)); //tour ici sinon boucle infini avec min max min etc..
+			for (int i = 0; i < 9; i++) {
+				for (int j = 0; j < 9; j++){
+					if (grille.isCoupPossible(i,j)){
+						BigGrille grillebis = grille.clone();
+						grillebis.ajouterCoup(i,j, symboleMin);
+						valeur = Math.min(valeur, this.max(par, grillebis, tour+1)); //tour ici sinon boucle infini avec min max min etc..
+					}
 				}
 			}
 			return valeur;
@@ -75,11 +81,13 @@ public class Minimax extends Algorithm {
 		}
 		else{
 			double valeur = Constantes.SCORE_MIN_NON_DEFINI;
-			for (int i = 0; i < Constantes.NB_COLONNES; i++) {
-				if (grille.isCoupPossible(i)){
-					Grille grillebis = grille.clone();
-					grillebis.ajouterCoup(i, symboleMax);
-					valeur = Math.max(valeur, this.min(grillebis, tour+1)); 
+			for (int i = 0; i < 9; i++) {
+				for (int j = 0; j < 9; j++){
+					if (grille.isCoupPossible(i,j)){
+						BigGrille grillebis = grille.clone();
+						grillebis.ajouterCoup(i,j, symboleMax);
+						valeur = Math.max(valeur, this.min(par, grillebis, tour+1)); 
+					}
 				}
 			}
 			return valeur;
